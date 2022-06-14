6a315936465cd46acffbcace5be37b5224153626
KannapicmS <88262676+KannapicmS@users.noreply.github.com>
2022-06-14 20:54:46 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,6 @@
-if [ -z $UPSTREAM_REPO ]
-then
-  echo "Cloning main Repository"
-  git clone https://github.com/KannapicmS/fasr.git /fasr
-else
-  echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /LuciferMoringstar-Robot
-fi
-cd /LuciferMoringstar-Robot
+echo "Cloning Repo...."
+git clone https://github.com/KannapicmS/fasr.git /fasr
+cd /fasr
 pip3 install -U -r requirements.txt
-echo "Starting LuciferMoringstar RoBot...."
+echo "Starting Bot...."
 python3 main.py
